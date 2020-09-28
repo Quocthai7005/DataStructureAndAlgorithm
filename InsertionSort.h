5fb706f4476ccae8556fb63b22ee5959da0b1665
Nguyen Quoc Thai <nguyenquocthai7005@gmail.com>
2020-09-28 23:01:08 +0700

    fix insertion sort

--- InsertionSort.h
+++ InsertionSort.h
@@ -4,7 +4,7 @@
 void insertionSort(int *array, int size) {
   for (int i = 0; i < size; i++) {
     int u = i;
-    while (array[u] < array[u-1] && array[u-1] >= 0) {
+    while (array[u] < array[u-1] && u-1 >= 0) {
       int temp = array[u-1];
       array[u-1] = array[u];
       array[u] = temp;
